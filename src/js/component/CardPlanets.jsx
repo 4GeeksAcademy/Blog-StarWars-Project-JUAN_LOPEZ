c07a1312cccfb905815db945639cc9f2f0285bf9
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-02-12 22:22:18 +0000

    finish

--- src/js/component/CardPlanets.jsx
+++ src/js/component/CardPlanets.jsx
@@ -7,58 +7,66 @@ const CardPlanets = () => {
 
   return (
     <>
-      <h3>Planets</h3>
-      <div className="d-flex justify-content-center">
-        {/* aqui empieza card para los planetas */}
-        <div className="container row row-cols-5">
+      <div className="container mt-4">
+        <h3 className="text-danger">Planets</h3>
+        <hr />
+      </div>
+      <div className="container py-2 overflow-auto">
+        {/* empieza el card */}
+        <div className="d-flex flex-row flex-nowrap">
           {store.planets.map((planet) => {
             return (
-              <div key={planet.uid} id="planets" className="container col">
-                <div className="mb-3">
-                  <div className="card">
-                    <div>
-                      <img
-                        src={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`}
-                        className="img-fluid rounded"
-                        alt={planet.properties.name}
-                      />
-                    </div>
-                    <div>
-                      <div className="card-body">
-                        <h5 className="card-title">{planet.properties.name}</h5>
-                        <p className="card-text">
-                          Diameter: {planet.properties.diameter}
-                        </p>
-                        <p className="card-text">
-                          Population: {planet.properties.population}
-                        </p>
-                        <p className="card-text">
-                          Gravity: {planet.properties.gravity}
-                        </p>
-                        <p className="card-text">
-                          climate: {planet.properties.climate}
-                        </p>
-                        <div className="card-text d-flex justify-content-between">
-                          <Link
-                            to={`/planets/${planet.uid}`}
-                            className="btn btn-success"
-                          >
-                            See details
-                          </Link>
-                          <button
-                            onClick={() => {
-                              actions.handleFavorite({name:planet.properties.name, _id: planet._id});
-                            }}
-                            className="btn btn-warning"
-                          >
-                            favorites
-                          </button>
-                        </div>
+              planet.uid != 1 && (
+                <div
+                  className="card mx-1"
+                  key={planet.uid}
+                  style={{ minWidth: "18rem" }}
+                >
+                  <div>
+                    <img
+                      src={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`}
+                      className="img-fluid rounded"
+                      alt={planet.properties.name}
+                    />
+                  </div>
+                  <div>
+                    <div className="card-body">
+                      <h5 className="card-title">{planet.properties.name}</h5>
+                      <p className="card-text">
+                        Diameter: {planet.properties.diameter}
+                      </p>
+                      <p className="card-text">
+                        Population: {planet.properties.population}
+                      </p>
+                      <p className="card-text">
+                        Gravity: {planet.properties.gravity}
+                      </p>
+                      <p className="card-text">
+                        climate: {planet.properties.climate}
+                      </p>
+                      <div className="card-text d-flex justify-content-between">
+                        <Link
+                          to={`/planets/${planet.uid}`}
+                          className="btn btn-success"
+                        >
+                          See details
+                        </Link>
+                        <button
+                          onClick={() => {
+                            actions.handleFavorite({
+                              name: planet.properties.name,
+                              _id: planet._id,
+                            });
+                          }}
+                          className="btn btn-warning"
+                        >
+                          favorites
+                        </button>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              )
             );
           })}
         </div>
